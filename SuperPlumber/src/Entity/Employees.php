19be95e2cf9b7ad3a67f7c6a933dc970d62a871f
Ilan Veneri <ilanven8@gmail.com>
2026-04-01 17:47:31 +0200

    CRUD entier + modif nom Availabilities + Début de fixtures

---
+++ SuperPlumber/src/Entity/Employees.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\Position;
+use App\Repository\EmployeesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: EmployeesRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+class Employees implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $lastName = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $phone = null;
+
+    #[ORM\Column(enumType: Position::class)]
+    private ?Position $position = null;
+
+    /**
+     * @var Collection<int, Interventions>
+     */
+    #[ORM\OneToMany(targetEntity: Interventions::class, mappedBy: 'fkEmployee')]
+    private Collection $interventions;
+
+    /**
+     * @var Collection<int, Availabilities>
+     */
+    #[ORM\OneToMany(targetEntity: Availabilities::class, mappedBy: 'fkEmployee', orphanRemoval: true)]
+    private Collection $availabilities;
+
+    public function __construct()
+    {
+        $this->interventions = new ArrayCollection();
+        $this->availabilities = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Ensure the session doesn't contain actual password hashes by CRC32C-hashing them, as supported since Symfony 7.3.
+     */
+    public function __serialize(): array
+    {
+        $data = (array) $this;
+        $data["\0".self::class."\0password"] = hash('crc32c', $this->password);
+
+        return $data;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): static
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getPosition(): ?Position
+    {
+        return $this->position;
+    }
+
+    public function setPosition(Position $position): static
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Interventions>
+     */
+    public function getInterventions(): Collection
+    {
+        return $this->interventions;
+    }
+
+    public function addIntervention(Interventions $intervention): static
+    {
+        if (!$this->interventions->contains($intervention)) {
+            $this->interventions->add($intervention);
+            $intervention->setFkEmployee($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIntervention(Interventions $intervention): static
+    {
+        if ($this->interventions->removeElement($intervention)) {
+            // set the owning side to null (unless already changed)
+            if ($intervention->getFkEmployee() === $this) {
+                $intervention->setFkEmployee(null);
+            }
+        }
+
+        return $this;
+    }
+
+
+
+    /**
+     * @return Collection<int, Availabilities>
+     */
+    public function getAvailabilities(): Collection
+    {
+        return $this->availabilities;
+    }
+
+    public function addAvailability(Availabilities $availability): static
+    {
+        if (!$this->availabilities->contains($availability)) {
+            $this->availabilities->add($availability);
+            $availability->setFkEmployee($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAvailability(Availabilities $availability): static
+    {
+        if ($this->availabilities->removeElement($availability)) {
+            // set the owning side to null (unless already changed)
+            if ($availability->getFkEmployee() === $this) {
+                $availability->setFkEmployee(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ SuperPlumber/src/Entity/Pieces.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PiecesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PiecesRepository::class)]
+class Pieces
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\Column]
+    private ?int $alertTreshold = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $supplier = null;
+
+    /**
+     * @var Collection<int, UsedPieces>
+     */
+    #[ORM\ManyToMany(targetEntity: UsedPieces::class, mappedBy: 'fkPiece')]
+    private Collection $usedPieces;
+
+    public function __construct()
+    {
+        $this->usedPieces = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getAlertTreshold(): ?int
+    {
+        return $this->alertTreshold;
+    }
+
+    public function setAlertTreshold(int $alertTreshold): static
+    {
+        $this->alertTreshold = $alertTreshold;
+
+        return $this;
+    }
+
+    public function getSupplier(): ?string
+    {
+        return $this->supplier;
+    }
+
+    public function setSupplier(string $supplier): static
+    {
+        $this->supplier = $supplier;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, UsedPieces>
+     */
+    public function getUsedPieces(): Collection
+    {
+        return $this->usedPieces;
+    }
+
+    public function addUsedPiece(UsedPieces $usedPiece): static
+    {
+        if (!$this->usedPieces->contains($usedPiece)) {
+            $this->usedPieces->add($usedPiece);
+            $usedPiece->addFkPiece($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUsedPiece(UsedPieces $usedPiece): static
+    {
+        if ($this->usedPieces->removeElement($usedPiece)) {
+            $usedPiece->removeFkPiece($this);
+        }
+
+        return $this;
+    }
+}
