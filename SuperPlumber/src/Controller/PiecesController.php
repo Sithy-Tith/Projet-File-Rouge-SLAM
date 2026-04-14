908a2284548867ce1a3799fe55f693ea386f6dde
Ilan Veneri <ilanven8@gmail.com>
2026-04-14 16:04:02 +0200

    Pieces/Intervention : affiche le stock des pièces avec les stocks faibles en rouge

--- SuperPlumber/src/Controller/PiecesController.php
+++ SuperPlumber/src/Controller/PiecesController.php
@@ -6,7 +6,6 @@ use App\Entity\Pieces;
 use App\Form\PiecesType;
 use App\Repository\PiecesRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use phpDocumentor\Reflection\Types\Boolean;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -43,7 +42,7 @@ final class PiecesController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_pieces_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_pieces_show', methods: ['GET'], requirements: ['id' => '\d+'])]
     public function show(Pieces $piece): Response
     {
         return $this->render('pieces/show.html.twig', [
@@ -69,7 +68,7 @@ final class PiecesController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_pieces_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_pieces_delete', methods: ['POST'], requirements: ['id' => '\d+'])]
     public function delete(Request $request, Pieces $piece, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$piece->getId(), $request->getPayload()->getString('_token'))) {
@@ -86,13 +85,13 @@ final class PiecesController extends AbstractController
 
 // Affiche l'inventaire de toutes les pièces avec leurs stock
 
-#[Route('/inventaire/' , name: 'app_pieces_inventory', methods: ['GET'])]
-    public function inventory(PiecesRepository $piecesRepository, Boolean $edition=False): Response
+#[Route('/inventaire' , name: 'app_pieces_inventory', methods: ['GET'])]
+    public function inventory(PiecesRepository $piecesRepository, bool $edition= false): Response
     {
         $pieces = $piecesRepository->findAll();
         foreach($pieces as $piece){
             // Si la pièce possède un stock inférieur à son seuil d'alerte
-            if ($piece->getQuantity()<=$piece->getAlertTreshold()){
+            if ($piece->getQuantity()<= $piece->getAlertTreshold()){
                 $alertPieces[]=$piece;
             }else{
                 $normalPieces[]=$piece;
