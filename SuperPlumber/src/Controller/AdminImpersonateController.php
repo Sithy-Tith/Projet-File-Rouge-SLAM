97d9ed299cd586fba368be621f6f4ffdc9afad78
Sithy-Tith <nuildtk@gmail.com>
2026-05-11 11:51:20 +0000

    Ajout du nouveau controller admin pour impersonate

---
+++ SuperPlumber/src/Controller/AdminImpersonateController.php
@@ -0,0 +1,22 @@
+namespace App\Controller;
+
+use App\Entity\Employees;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AdminController extends AbstractController
+{
+    #[Route('/admin/impersonate', name: 'admin_impersonate')]
+    public function impersonate(EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $users = $em->getRepository(Employees::class)->findAll();
+
+        return $this->render('admin/impersonate.html.twig', [
+            'users' => $users
+        ]);
+    }
+}
