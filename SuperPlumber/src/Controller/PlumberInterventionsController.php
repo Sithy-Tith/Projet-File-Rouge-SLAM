5ace4aebf06393da6432ee31cd8d9ca889cf983b
Ilan Veneri <ilanven8@gmail.com>
2026-05-20 14:18:52 +0200

    Création du index() du  PlumberInterventionsController

--- SuperPlumber/src/Controller/PlumberInterventionsController.php
+++ SuperPlumber/src/Controller/PlumberInterventionsController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Interventions;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PlumberInterventionsController extends AbstractController
 {
     #[Route('/plumber/interventions', name: 'app_plumber_interventions')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $myInterventions = $em->getRepository(Interventions::class)->findBy(
+            ['fkEmployee' => $this->getUser()],
+            ['date' => 'DESC'],
+        );
         return $this->render('plumber_interventions/index.html.twig', [
-            'controller_name' => 'PlumberInterventionsController',
+            // 'controller_name' => 'PlumberInterventionsController', (on garde ça peut être utile)
+            'interventions' => $myInterventions,
         ]);
     }
 }
