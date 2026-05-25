236154c0cf550d721182ba20ebdc6e08e13baf1c
Ilan Veneri <ilanven8@gmail.com>
2026-05-25 10:50:31 +0200

    Création d'une clé de traduction + fichier de traduction pour l'enum Status

--- SuperPlumber/src/Enum/Status.php
+++ SuperPlumber/src/Enum/Status.php
@@ -10,4 +10,11 @@ enum Status: string
     case FINISHED = 'finished';
     case CANCELED = 'canceled';
 
+    // Créer une clé pour Symfony qui sera liée à la value de chaque enum. Ex :
+    // status.to_plan -> 'to_plan'
+    // Cette clé pourra être utilisée pour proposer des traductions sur ce champs dans /translations/messages.fr.yaml
+    public function label(): string
+    {
+        return 'status.' . $this->value;
+    }
 }
