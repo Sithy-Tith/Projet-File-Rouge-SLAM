d1f70aeb20033b9e88097cc833d1df8a338735b6
Sithy-Tith <nuildtk@gmail.com>
2026-06-06 12:04:11 +0200

    Correction de l'affichage des interventions, champ date

--- SuperPlumber/src/Controller/ClientDashboardController.php
+++ SuperPlumber/src/Controller/ClientDashboardController.php
@@ -50,7 +50,7 @@ final class ClientDashboardController extends AbstractController
 
         $mesInterventions = $em->getRepository(Interventions::class)->findBy(
             ['fkClient' => $this->getUser()],
-            ['date' => 'ASC']
+            ['startAt' => 'ASC']
         );
 
         return $this->render('clients/dashboard.html.twig', [
@@ -63,7 +63,7 @@ final class ClientDashboardController extends AbstractController
     {
         $mesInterventions = $em->getRepository(Interventions::class)->findBy(
             ['fkClient' => $this->getUser()],
-            ['date' => 'DESC']
+            ['startAt' => 'DESC']
         );
 
         return $this->render('clients/interventions_list.html.twig', [
